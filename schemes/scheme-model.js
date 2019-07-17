e79b65f16fc21650c425af0fd9ae412d066e16f3
Nabeelah <nabeelahyousuph@gmail.com>
2019-07-17 15:37:33 +0100

    Create method to get scheme steps in order

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,7 +2,8 @@ const db = require("../data/dbConfig");
 
 module.exports = {
   find,
-  findById
+  findById,
+  findSteps
 };
 
 function find() {
@@ -11,4 +12,16 @@ function find() {
 
 function findById(id) {
   return db("schemes").where({ id });
+}
+
+function findSteps(id) {
+  return db
+    .select(
+      "steps.id", "steps.instructions",
+      "schemes.scheme_name", "steps.step_number"
+    )
+    .from("schemes")
+    .where("steps.scheme_id", id)
+    .innerJoin("steps", "steps.scheme_id", "schemes.id")
+    .orderBy('steps.step_number', "asc");
 }
