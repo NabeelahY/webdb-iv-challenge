fe63e375c066064770f4d6ed00133a72f7f3e546
Nabeelah <nabeelahyousuph@gmail.com>
2019-07-17 14:54:58 +0100

    Create helper functions to get schemes

---
+++ schemes/scheme-model.js
@@ -0,0 +1,14 @@
+const db = require("../data/dbConfig");
+
+module.exports = {
+  find,
+  findById
+};
+
+function find() {
+  return db("schemes");
+}
+
+function findById(id) {
+  return db("schemes").where({ id });
+}
